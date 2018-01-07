3837ed7a13a169b850a750464791a61e6b2e83b4
Witek85 <witold.szklarski@gmail.com>
2018-01-08 00:32:29 +0100

    excludes begins or ends with 3

--- js/arrays.js
+++ js/arrays.js
@@ -27,13 +27,13 @@ export function cleanWhitespaces(arr) {
 // - jest a, bc, d
 // - ma być a,b,c,d
 
-	var newArrConcat = [];
-	var newArr = arr.slice(0);
-	for (let i = 0; i < newArr.length; i++) {
-		newArr[i] = newArr[i].replace(/\s/g,'');
-		newArr[i] = newArr[i].split("");
-		newArrConcat = newArrConcat.concat(newArr[i]);
-	}
+var newArrConcat = [];
+var newArr = arr.slice(0);
+for (let i = 0; i < newArr.length; i++) {
+	newArr[i] = newArr[i].replace(/\s/g,'');
+	newArr[i] = newArr[i].split("");
+	newArrConcat = newArrConcat.concat(newArr[i]);
+}
 	// console.log(arr);
 	// console.log(newArr);
 	// console.log(newArrConcat);
@@ -42,27 +42,29 @@ export function cleanWhitespaces(arr) {
 
 export function randomValues() {
 
-let arr = [];
-const aLimit = 1000;
-const rand = (param) => {return Math.floor((Math.random()*param)+1)};
+	let arr = [];
+	const aLimit = 1000;
+	const rand = (param) => {return Math.floor((Math.random()*param)+1)};
 
-for (let b = 0; b < aLimit; b++) {
+	for (let b = 0; b < aLimit; b++) {
 
-	let customer = {
-  	customer_id: rand(100),
-    apple: rand(10),
-    banana: rand(10),
-    melon: rand(10)
-  }
-	arr.push(customer);
-}
+		let customer = {
+			customer_id: rand(100),
+			apple: rand(10),
+			banana: rand(10),
+			melon: rand(10)
+		}
+		arr.push(customer);
+	}
 
-console.log(arr);
+	console.log(arr);
 
 /*profit per fruit:
 apple = 10c
 banana = 20c
 melon = 50c */
+
+// TODO excluding customer which id starts or ends with number 3
 
 let profitApple = 10;
 let profitBanana = 20;
@@ -70,41 +72,46 @@ let profitMelon = 50;
 
 let uniqueArrIds = [];
 let uniqueArr = [];
+const regex = /(^3|3$)/;
+
 for (let c = 0; c < arr.length; c++) {
-	if(uniqueArrIds.includes(arr[c].customer_id) ) {
 
-		for (let d = 0; d < uniqueArr.length; d++) {
-			if (uniqueArr[d].customer_id === arr[c].customer_id) {
+	if (!regex.test(arr[c].customer_id)) {
+	// alt way
+	// if (arr[c].customer_id.toString().charAt(0) === "3" || arr[c].customer_id.toString().charAt(arr[c].customer_id.toString().length - 1) === "3") {}
 
-				uniqueArr[d].orders += 1;
+		if(uniqueArrIds.includes(arr[c].customer_id) ) {
 
-		    uniqueArr[d].appleTotal += arr[c].apple;
-		    uniqueArr[d].bananaTotal += arr[c].banana;
-		    uniqueArr[d].melonTotal += arr[c].melon;
+			for (let d = 0; d < uniqueArr.length; d++) {
+				if (uniqueArr[d].customer_id === arr[c].customer_id) {
 
-				uniqueArr[d].appleProfit += (arr[c].apple * profitApple);
-				uniqueArr[d].bananaProfit += (arr[c].banana * profitBanana);
-				uniqueArr[d].melonProfit += (arr[c].melon * profitMelon);
+					uniqueArr[d].orders += 1;
 
+					uniqueArr[d].appleTotal += arr[c].apple;
+					uniqueArr[d].bananaTotal += arr[c].banana;
+					uniqueArr[d].melonTotal += arr[c].melon;
+
+					uniqueArr[d].appleProfit += (arr[c].apple * profitApple);
+					uniqueArr[d].bananaProfit += (arr[c].banana * profitBanana);
+					uniqueArr[d].melonProfit += (arr[c].melon * profitMelon);
+				}
 			}
+		} else {
+
+			let customer = {
+				customer_id: arr[c].customer_id,
+				orders: 1,
+				appleTotal: arr[c].apple,
+				bananaTotal: arr[c].banana,
+				melonTotal: arr[c].melon,
+				appleProfit: arr[c].apple * profitApple,
+				bananaProfit: arr[c].banana * profitBanana,
+				melonProfit: arr[c].melon * profitMelon
+			}
+			uniqueArr.push(customer);	
+			uniqueArrIds.push(arr[c].customer_id);
 		}
-
-	} else {
-
-	let customer = {
-  	customer_id: arr[c].customer_id,
-    orders: 1,
-    appleTotal: arr[c].apple,
-    bananaTotal: arr[c].banana,
-    melonTotal: arr[c].melon,
-    appleProfit: arr[c].apple * profitApple,
-    bananaProfit: arr[c].banana * profitBanana,
-    melonProfit: arr[c].melon * profitMelon
-  }
-		uniqueArr.push(customer);	
-		uniqueArrIds.push(arr[c].customer_id);
 	}
-	
 }
 
 console.log(uniqueArr);
